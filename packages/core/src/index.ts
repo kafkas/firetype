12eb3196f0fbb8aeb204af55df45868044d71526
Anar Kafkas <anarkafkas@gmail.com>
2020-10-18 15:16:52 +0300

    Get TS to compile successfully

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
+/**
+ * @param num - A number
+ * @returns The square of the specified number.
+ */
 export function square(num: number) {
   return num * num;
 }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,7 @@
+import { square } from "@firetype/core";
+
+console.log(square(4));
+
 export function concat(str1: string, str2: string) {
   return str1 + str2;
 }
